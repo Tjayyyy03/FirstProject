b0bad538ebaf7bee901ceaf0af28565e5f7e538c
Tjayyyy03 <dejesustomjustine@gmail.com>
2026-02-14 12:56:14 +0800

    Add files via upload

---
+++ DejesusProject.cs
@@ -0,0 +1,31 @@
+﻿namespace DejesusFirstProject
+{
+
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+                Console.WriteLine("    LOGIN     ");
+                string user = "tomjustine";
+                string pass = "bsitoverallchampion123";
+                Console.Write("Enter username: ");
+                string inputUser = Console.ReadLine();
+
+                Console.Write("Enter password: ");
+                string inputPassword = Console.ReadLine();  
+
+                if (inputUser == user && inputPassword == pass)
+                {
+                    Console.WriteLine("Login Successful! Welcome.");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password! Please Try Again.");
+                }
+            }
+        }
+    }
+
